7d7b320cfc94ea7bd130bbb96363cc87a2f2960b
LuisCamino <luiscm4c@gmail.com>
2025-07-03 17:54:47 +0200

    añadidas salsas y mejorado la legibilidad del resto de elementos e implementado boton de desplazamiento hacia arriba

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-/*
-<a href="https://ibb.co/XZqS773V"><img src="https://i.ibb.co/997sbbVG/agua.jpg" alt="agua" border="0"></a>
-<a href="https://ibb.co/nqN6X6my"><img src="https://i.ibb.co/LDXYTYRy/botella-aquarius-limon.jpg" alt="botella-aquarius-limon" border="0"></a>
-<a href="https://ibb.co/zh43904h"><img src="https://i.ibb.co/kgSLbWSg/botella-aquarius-naranja.jpg" alt="botella-aquarius-naranja" border="0"></a>
-<a href="https://ibb.co/zHjzs449"><img src="https://i.ibb.co/Lhb3SZZj/botella-coca-cola.jpg" alt="botella-coca-cola" border="0"></a>
-<a href="https://ibb.co/pBdSHh25"><img src="https://i.ibb.co/VcTsFwV5/botella-coca-cola-zero.jpg" alt="botella-coca-cola-zero" border="0"></a>
-<a href="https://ibb.co/mrMtwjqM"><img src="https://i.ibb.co/LD26WFt2/botella-coca-cola-zero-zero.jpg" alt="botella-coca-cola-zero-zero" border="0"></a>
-<a href="https://ibb.co/PvP9s7s8"><img src="https://i.ibb.co/v6TQC7C8/botella-fanta-limon.jpg" alt="botella-fanta-limon" border="0"></a>
-<a href="https://ibb.co/0j2CTp9V"><img src="https://i.ibb.co/27hvx0MY/botella-fanta-naranja.jpg" alt="botella-fanta-naranja" border="0"></a>
-<a href="https://ibb.co/PGwzJhpb"><img src="https://i.ibb.co/LdnhBtwq/botella-fuze-tea-limon.jpg" alt="botella-fuze-tea-limon" border="0"></a>
-<a href="https://ibb.co/XfbZrGXG"><img src="https://i.ibb.co/zWrhTtst/botella-fuze-tea-maracuya.jpg" alt="botella-fuze-tea-maracuya" border="0"></a>
-<a href="https://ibb.co/mr6d6Srx"><img src="https://i.ibb.co/PswJwtsS/botella-sprite.jpg" alt="botella-sprite" border="0"></a>
-<a href="https://ibb.co/gZLBkFNH"><img src="https://i.ibb.co/0yRdHVT8/lata-aquarius-limon.jpg" alt="lata-aquarius-limon" border="0"></a>
-<a href="https://ibb.co/jZbwFnKq"><img src="https://i.ibb.co/1J7XxFVB/lata-aquarius-naranja.jpg" alt="lata-aquarius-naranja" border="0"></a>
-<a href="https://ibb.co/8gx3qtHP"><img src="https://i.ibb.co/CK9dgDfV/lata-coca-cola.jpg" alt="lata-coca-cola" border="0"></a>
-<a href="https://ibb.co/WNZrWLh6"><img src="https://i.ibb.co/Ng506Mvs/lata-coca-cola-zero.jpg" alt="lata-coca-cola-zero" border="0"></a>
-<a href="https://ibb.co/YBjnbLLs"><img src="https://i.ibb.co/r2y8vwwr/lata-coca-cola-zero-zero.jpg" alt="lata-coca-cola-zero-zero" border="0"></a>
-<a href="https://ibb.co/1t5kJXBs"><img src="https://i.ibb.co/Rkmq46th/lata-fuze-tea-limon.jpg" alt="lata-fuze-tea-limon" border="0"></a>
-<a href="https://ibb.co/Mmrj1vQ"><img src="https://i.ibb.co/FF2fYrC/lata-fuze-tea-maracuya.jpg" alt="lata-fuze-tea-maracuya" border="0"></a>
-*/
+
 
 const buttons = [
   {
@@ -159,11 +140,93 @@ const buttons = [
     type: 'botella',
     image: 'https://i.ibb.co/997sbbVG/agua.jpg',
     color: 'black'
+  },
+];
+
+const SALSA_NAME = [
+  {
+    name: 'ketchup',
+    backgroundColor: 'red'
+  },
+  {
+    name: 'mayonesa',
+    backgroundColor: '#f9f4eb'
+  },
+  {
+    name: 'barbacoa',
+    backgroundColor: '#7d2d21'
+  },
+  {
+    name: 'trufa',
+    backgroundColor: '#c1bd91'
+  },
+  {
+    name: 'cheddar',
+    backgroundColor: '#ef9202'
+  },
+  {
+    name: 'bacon',
+    backgroundColor: '#be6f6a'
+  },
+  {
+    name: 'ranchera',
+    backgroundColor: '#d0cab3'
   }
 ];
 
+function TwoColumnGroups({ items }) {
+  const groupSize = 2;
+  const columns = Math.ceil(items.length / groupSize);
+
+  const cols = Array.from({ length: columns }, (_, colIndex) =>
+    items.slice(colIndex * groupSize, colIndex * groupSize + groupSize)
+  );
+
+  const rows = groupSize;
+
+  return (
+    <div style={{ display: 'flex', gap: '20px', padding: '10px' }}>
+      {[...Array(rows)].map((_, rowIndex) => (
+        <div
+          key={rowIndex}
+          style={{
+            padding: '10px',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '8px',
+            textAlign: 'left',
+            padding: '0 5px',
+          }}
+        >
+          {cols.map((col, colIndex) => (
+            <div key={colIndex} style={{ padding: '4px 8px' }}>
+              {col[rowIndex] || null}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+
 function App() {
+
   const [counts, setCounts] = useState({});
+  const [showScrollTop, setShowScrollTop] = useState(false);
+
+  // Detectar scroll y mostrar botón si scrollY > 100
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScrollTop(window.scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Limpieza del event listener al desmontar el componente
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   const handleAdd = (name) => {
     setCounts((prev) => ({
@@ -172,34 +235,62 @@ function App() {
     }));
   };
 
-  const handleRemoveOne = (name) => {
-    setCounts((prev) => ({
-      ...prev,
-      [name]: Math.max((prev[name] || 0) - 1, 0),
-    }));
-  };
-
   const handleReset = () => {
     setCounts({});
   };
 
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   const getResultsText = () => {
-    const grouped = { lata: [], botella: [] };
+    const grouped = { lata: [], botella: [], otro: [] };
 
     for (const { name, type } of buttons) {
       if (counts[name]) {
-        const displayName = name.replace(/\s(33|50)cl$/, ''); // Elimina " 33cl" o " 50cl"
-        grouped[type].push(`${displayName} x${counts[name]}`);
+        grouped[type].push(`${name.replace(/ ?(33cl|50cl)/g, '')} x${counts[name]}`);
+      }
+    }
+
+    for (const { name } of SALSA_NAME) {
+      if (counts[name]) {
+        grouped.otro.push(`${name} x${counts[name]}`);
       }
     }
 
     return (
       <>
-        <p><strong>Latas:</strong><br />{grouped.lata.join(', ')}</p>
-        <p><strong>Botellas:</strong><br />{grouped.botella.join(', ')}</p>
+        {grouped.lata.length > 0 && (
+          <>
+            <strong>Latas:</strong>
+            <TwoColumnGroups items={grouped.lata} />
+          </>
+        )}
+        {grouped.botella.length > 0 && (
+          <>
+            <strong>Botellas:</strong>
+            <TwoColumnGroups items={grouped.botella} />
+          </>
+        )}
+        {grouped.otro.length > 0 && (
+          <>
+            <strong>Salsas:</strong>
+            <TwoColumnGroups items={grouped.otro} />
+          </>
+        )}
       </>
     );
   };
+
+  // Función para dividir array en chunks
+  const chunkArray = (arr, size) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+    }
+    return result;
+  };
+
 
   const renderButtonsByType = (type) => (
     <div className="button-group">
@@ -242,6 +333,7 @@ function App() {
         style={{ width: '150px', height: 'auto' }}
       />
       <h3>Delivery app</h3>
+
       <div className="results" style={{
         border: "2px solid black",
         margin: "20px",
@@ -256,13 +348,98 @@ function App() {
       }}>
         {getResultsText()}
       </div>
+
       <div className="actions">
-        <button onClick={handleReset}>reset</button>
+        <img
+          src="https://imgs.search.brave.com/JlAoMqux6hH9Ixgrxm3oCbv4I5WOhWF1MIWp_iux0jE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8zNjI1LzM2MjUw/MDUucG5nP3NlbXQ9/YWlzX2h5YnJpZA"
+          alt="Reset"
+          onClick={handleReset}
+          style={{
+            cursor: 'pointer',
+            width: '32px',
+            height: '32px',
+            margin: '8px'
+          }}
+        />
       </div>
 
       {renderButtonsByType('lata')}
       {renderButtonsByType('botella')}
 
+      <div
+        onClick={() => handleAdd(SALSA_NAME)}
+        style={{
+          display: 'inline-block',
+
+          position: 'relative',
+          margin: '20px',
+          cursor: 'pointer',
+        }}
+        title="Añadir salsa"
+      >
+        <div className="button-group">
+          <h2>Salsas</h2>
+          <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
+            {SALSA_NAME.map(({ name, backgroundColor }) => (
+              <div
+                key={name}
+                style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', cursor: 'pointer' }}
+                title={name}
+                onClick={() => handleAdd(name)}
+              >
+                <div
+                  style={{
+                    width: '25px',
+                    height: '25px',
+                    borderLeft: '4px solid black',
+                    borderRight: '4px solid black',
+                    borderBottom: '4px solid black',
+                    position: 'relative',
+                    cursor: 'pointer',
+                  }}
+
+                  
+                >
+                  <div
+                    style={{
+                      width: '20px',
+                      height: '20px',
+                      backgroundColor: backgroundColor,
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                    }}
+                  ></div>
+                </div>
+                <span style={{ fontSize: '12px', marginTop: '4px', userSelect: 'none' }}>{name}</span>
+              </div>
+            ))}
+          </div>
+          {showScrollTop && (
+            <button
+              onClick={scrollToTop}
+              style={{
+                position: 'fixed',
+                bottom: '20px',
+                right: '20px',
+                padding: '10px 15px',
+                fontSize: '14px',
+                cursor: 'pointer',
+                borderRadius: '5px',
+                border: 'none',
+                backgroundColor: '#ea3142',
+                color: 'white',
+                boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
+                zIndex: 1000,
+              }}
+              aria-label="Ir arriba"
+            >
+              ↑ 
+            </button>
+          )}
+        </div>
+      </div>
 
     </div>
   );
